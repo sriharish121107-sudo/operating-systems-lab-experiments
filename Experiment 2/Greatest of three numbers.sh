5a998ad6197626f0c9b7a6ba93deda939193ad16
SRIHARISHJP2007 <sriharish121107@gmail.com>
2026-08-18 18:58:42 +0530

    Add files via upload

---
+++ Experiment 2/Greatest of three numbers.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "ENTER THREE NUMBERS"
+read a b c
+
+if [ $a -gt $b ] && [ $a -gt $c ]
+then
+    echo "$a is greater"
+
+elif [ $b -gt $c ]
+then
+    echo "$b is greater"
+
+else
+    echo "$c is greater"
+fi
